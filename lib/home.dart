e63172e9bec52e331f7c9b2018318e2e1eb51224
ESRAA <esraagamal1909@gmail.com>
2021-11-01 17:54:57 +0200

    finish homeScreen add tabs

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:islamy/main.dart';
+import 'package:islamy/moshaf/moshafTab.dart';
+import 'package:islamy/quran/quranTabe.dart';
+import 'package:islamy/sebha/sebhaTab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName='home';
 
-  Widget build(BuildContext context) {
-    return Scaffold(
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex=0;
+
+  Widget build(BuildContext context) {
+    return Stack(
+      children:[
+        Image.asset('asset/image/bg3@3x.png'),
+
+       Scaffold(
+         appBar: AppBar(
+           title: Text('islami'),
+         ),
+         bottomNavigationBar: Theme(
+           data: Theme.of(context).copyWith(
+             canvasColor: MyThemeData.PrimaryColor
+           ),
+           child: BottomNavigationBar(
+             onTap: (index){
+
+               currentIndex=index;
+               setState(() {
+
+               });
+             },
+
+             currentIndex:currentIndex ,
+             items: [
+               BottomNavigationBarItem(icon:ImageIcon(AssetImage('radio_blue.png')), label: 'radio'),
+               BottomNavigationBarItem(icon:ImageIcon(AssetImage('sebha_blue.png')), label: 'sebha'),
+               BottomNavigationBarItem(icon:ImageIcon(AssetImage('path 1.png')), label: 'quran'),
+               BottomNavigationBarItem(icon:ImageIcon(AssetImage('moshaf_blue.png')), label: 'moshaf'),
+
+             ],
+           ),
+         ),
+
+    body: Container(
+
+  child:Views[currentIndex] ,
+),
+
+      ),]
     );
   }
+  List<Widget>Views=[
+    Moshaf(),
+    Quran(),
+    Sebha(),
+
+
+  ];
 }
